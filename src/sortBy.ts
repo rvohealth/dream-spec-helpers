a8fb51895573d155d964db57a24844e8759a9a29
avocadojesus <jgarbey@gmail.com>
2025-03-06 15:04:36 -0800

    fixup

--- src/sortBy.ts
+++ src/sortBy.ts
@@ -3,17 +3,21 @@ export default function sortBy<T>(
   valueToCompare: (value: T) => number | string | bigint,
 ): T[] {
   return array.sort((a: T, b: T) => {
-    const aPrime = valueToCompare(a);
-    const bPrime = valueToCompare(b);
+    console.log({ a, b })
+    console.log(valueToCompare)
+    console.log(valueToCompare(a))
+    console.log(valueToCompare(b))
+    const aPrime = valueToCompare(a)
+    const bPrime = valueToCompare(b)
 
     if (typeof aPrime === "string" && typeof bPrime === "string")
-      return aPrime.localeCompare(bPrime);
+      return aPrime.localeCompare(bPrime)
     if (typeof aPrime === "number" && typeof bPrime === "number")
-      return aPrime - bPrime;
+      return aPrime - bPrime
     if (typeof aPrime === "bigint" && typeof bPrime === "bigint")
-      return aPrime.toString().localeCompare(bPrime.toString());
-    throw new UnsupportedValueFromComparisonFunction(aPrime, bPrime);
-  });
+      return aPrime.toString().localeCompare(bPrime.toString())
+    throw new UnsupportedValueFromComparisonFunction(aPrime, bPrime)
+  })
 }
 
 export class UnsupportedValueFromComparisonFunction extends Error {
@@ -21,10 +25,10 @@ export class UnsupportedValueFromComparisonFunction extends Error {
     private aPrime: any,
     private bPrime: any,
   ) {
-    super();
+    super()
   }
 
   public get message() {
-    return `Value incompatible with compare: ${this.aPrime}, ${this.bPrime}`;
+    return `Value incompatible with compare: ${this.aPrime}, ${this.bPrime}`
   }
 }
